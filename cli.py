79fa78ef3b9b92d31badb5775b71389acf004b5c
SuperKaninchen <16620882+SuperKaninchen@users.noreply.github.com>
2022-12-01 18:42:05 +0100

    outsource argument parsing to argparse

--- cli.py
+++ cli.py
@@ -1,24 +1,9 @@
 import sys
 import json
 import wsgi
+import argparse
 
 import board_calibration
-
-
-def parseArgs(args):
-    out = {}
-    i = 0
-    while i < len(args):
-        cur_arg = args[i]
-        if cur_arg.startswith("-"):
-            arg_name = cur_arg.lstrip("-")
-            i += 1
-            arg_value = args[i]
-            out[arg_name] = arg_value
-        else:
-            out["command"] = cur_arg
-        i += 1
-    return out
 
 
 def loadConfig(cfg):
@@ -26,16 +11,6 @@ def loadConfig(cfg):
         cfg_dict = json.loads(f.read())
     return cfg_dict
 
-
-def printHelp():
-    print("help text placeholder")
-
-
-def checkArgdict(argdict):  # Check if args contain necessary info
-    if not "i" in argdict:
-        sys.exit("Error: no cam index specified")
-    if not "a" in argdict:
-        sys.exit("Error: no cam api specified")
 
 if __name__ == "__main__":
 
@@ -46,33 +21,51 @@ if __name__ == "__main__":
     # Another (longer) example:
     # https://github.com/gandie/Ants/blob/master/bin/ants#L34
 
-    if len(sys.argv) == 1:
-        sys.exit("Error: missing arguments")
-    if sys.argv[1] in ["-h", "--help"]:  # User needs help
-        printHelp()
-        sys.exit()
+    parser = argparse.ArgumentParser(description="CLI for chess broadcasting")
 
-    argdict = parseArgs(sys.argv[1:])  # Parse all args except filename
+    parser.add_argument(  # For specifying a cam api
+        "command",
+        help="Specifies which subcommand to run",
+        choices=["test", "calibrate", "stream", "flask"]
+    )
 
-    if "cfg" in argdict:  # User has specified a config file to read
-        config = loadConfig(argdict["cfg"])
+    parser.add_argument(  # For using a config file
+        "-c",
+        "--config",
+        help="Specifies path to a config file"
+    )
+
+    parser.add_argument(  # For specifying a cam index
+        "-i",
+        "--index",
+        help="Specifies camera index"
+    )
+
+    parser.add_argument(  # For specifying a cam api
+        "-a",
+        "--api",
+        help="Specifies camera API",
+        choices=["any", "l4v2"]
+    )
+
+    args = parser.parse_args()
+
+    if args.config:  # User has specified a config file to read
+        config = loadConfig(args.config)
     else:
-        checkArgdict(argdict)
+        config = {}
         config["cam"] = {
-            "index": argdict["i"],
-            "api": argdict["a"]
+            "index": args.index,
+            "api": args.api
         }
 
-    if not "command" in argdict:  # User hasn't specified any subcommand
-        sys.exit("Error: no command specified!")
-
-    if argdict["command"] == "flask":
+    if args.command == "flask":
         wsgi.runApp(config["flask"])  # Run flask app with custom config
 
-    elif argdict["command"] == "stream":
+    elif args.command == "stream":
         pass  # Run image recognition and send updates to flask
 
-    elif argdict["command"] == "calibrate":
+    elif args.command == "calibrate":
         board_calibration.calibrate(config["cam"])
 
     else:
